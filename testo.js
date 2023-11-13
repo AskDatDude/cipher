0384ce4b715385ac2aaaad39212c36c5c68a1e2c
Sibuli <144430332+Sibuli@users.noreply.github.com>
2023-11-13 13:29:14 +0200

    piste

--- testo.js
+++ testo.js
@@ -1,6 +1,16 @@
-Key1{
-  "lowercase": "abcdefghijklmnopqrstuvwxyz",
-  "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  "special_characters": "!@#$%^&*()-_=+[{]};:'\",<.>/?`~",
-  "numbers": "0123456789"
+function calculate(){
+  let textInput = document.getElementById("textInput").value;
+  let result = splitText(textInput);
+  console.log(result);
 }
+  
+  function loopperi() {
+      for (let i = 0; i < textInput.length; i++) {
+      }
+  }
+let key1 = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+  "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", ",", ":", ";", "'", '"', "<", ".", ">", "/", "?", "`", "~",
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+];
